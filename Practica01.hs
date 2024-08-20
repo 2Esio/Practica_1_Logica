24b42e4aedfb5cb1f0ea4c3f58e2e41ec04e4e98
Dani-Mol <daniel.molina@comunidad.unam.mx>
2024-08-19 23:54:06 -0600

    Ejercicio 2

--- Practica01.hs
+++ Practica01.hs
@@ -1,8 +1,10 @@
--- Shape 
+
+-- 1. Tipos de datos Algebraicos
+
+-- 1.1 Shape 
 
 data Shape = Circle Float | Square Float | Rectangle Float Float
         | Triangle Float Float | Trapeze Float Float Float deriving (Show, Eq)
-
 
 area :: Shape -> Float 
 area (Circle r) = pi * r ** 2 
@@ -18,8 +20,17 @@ perimeter (Rectangle l w) = 2 * (l + w)
 perimeter (Triangle b h) = b * 3
 perimeter (Trapeze a b h) = a + b + 2 * sqrt ((abs(a - b)/2)^2 + h^2)
 
-
-
 instance Ord Shape where
     compare :: Shape -> Shape -> Ordering
     compare s1 s2 = compare (area s1) (area s2)
+
+
+-- 1.2 Point
+
+type Point = (Float, Float)
+
+distance :: Point -> Point -> Float
+distance (x_1, y_1) (x_2, y_2)= sqrt((x_2 - x_1)**2 +(y_2 - y_1)**2)
+
+from0 :: Point -> Float 
+from0 (x, y) = distance (0,0) (x,y) 
