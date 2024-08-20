fd173b322dbd0ae250e6853fdb38d35bb6baf925
etnicst <94811143+etnicst@users.noreply.github.com>
2024-08-19 23:45:49 -0600

    Ejercicio 4.4

--- Practica01.hs
+++ Practica01.hs
@@ -23,3 +23,26 @@ perimeter (Trapeze a b h) = a + b + 2 * sqrt ((abs(a - b)/2)^2 + h^2)
 instance Ord Shape where
     compare :: Shape -> Shape -> Ordering
     compare s1 s2 = compare (area s1) (area s2)
+
+
+
+
+
+{- Ejercicio 4.4: 
+Implementar usando foldr la función reversaFr, la cuál dada una lista nos regresa la lista con
+ los mismos elementos pero en orden opuesto.-}
+
+{- Función aux que usaremos en reversaFr con foldr
+Recibe un elemento x y una lista xs,
+regresa la lista con el elemento concatenado al final -}
+agregarFinal :: a -> [a] -> [a]
+agregarFinal x xs = xs ++ [x]
+
+{- Función principal que usa foldr en la lista de entrada
+(comenzará con la lista vacía) y se utiliza la concatenación.
+El elemento x se coloca al final de la lista xs y se concatenan.
+Foldr aplicara a cada elemento de la lista nuestra función auxiliar,
+para así, construir la lista al revés-}
+reversaFr :: [a] -> [a]
+reversaFr = foldr agregarFinal []
+    
