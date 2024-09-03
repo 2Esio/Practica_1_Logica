9d362cd51bd4bfc7725057baffa59a9216244851
2Esio <marco@localhost.localdomain>
2024-09-03 14:07:57 -0600

    Documentación de practica2.hs | 2Esio | 03/09/2024 14:07

--- practica2.hs
+++ practica2.hs
@@ -1,3 +1,4 @@
+-- Pre - código --
 data Prop = Var String | Cons Bool | Not Prop
  | And Prop Prop | Or Prop Prop
  | Impl Prop Prop | Syss Prop Prop deriving (Eq)
@@ -23,6 +24,12 @@ u = Var "u"
 
 type Estado = [String]
 
+-- Termina pre - código --
+--
+-- Inicia funciones --  
+
+-- E1 Definir la función variables :: Prop -> [String] tal que variables f devuelve el conjunto formado por todas las variables proposicionales que aparecen en f. 
+
 eliminarRepetidos :: (Eq a)=> [a]-> [a]
 eliminarRepetidos [ ] = [ ]
 eliminarRepetidos (x:xs) = if x `elem` xs then eliminarRepetidos xs else [x] ++ eliminarRepetidos xs
@@ -36,10 +43,13 @@ variables (Or p q) = eliminarRepetidos(variables p ++ variables q)
 variables (Impl p q) = eliminarRepetidos(variables p ++ variables q)
 variables (Syss p q) = eliminarRepetidos(variables p ++ variables q)
 
+-- E2 Definir la función conjPotencia :: [a] -> [[a]] tal que conjPotencia x devuelve la lista de todos los subconjuntos de x.
 
 conjPotencia :: [a] -> [[a]]
 conjPotencia [] = [[]]
 conjPotencia (x:xs) = [(x:ys) | ys <- conjPotencia xs] ++ conjPotencia xs
+
+-- E3 Definir la función interpretacion :: Prop -> Estado -> Bool tal que interpretacion f i es la interpretacion de f bajo i.
 
 interpretacion :: Prop -> Estado -> Bool
 interpretacion (Var v) xs = v `elem` xs 
@@ -50,6 +60,7 @@ interpretacion (Or f1 f2) xs = interpretacion f1 xs || interpretacion f2 xs
 interpretacion (Impl f1 f2) xs = interpretacion (Not f1) xs || interpretacion f2 xs
 interpretacion (Syss f1 f2) xs = interpretacion (Impl f1 f2) xs && interpretacion (Impl f2 f1) xs
 
+-- E4 Definir una función que dada una fórmula proposicional, la función devuelve los estados con los que podemos evaluar la fórmula.
 
 sustituir :: String -> Prop -> Prop -> Prop
 sustituir x sub (Var y) = if x == y then sub else Var y
@@ -79,7 +90,19 @@ contar (Or p q) = (contar p) +(contar q)
 contar (Impl p q) = (contar p) +(contar q)
 contar (Syss p q) = (contar p) +(contar q)
 
+-- E5 Definir una función que dada una fórmula proposicional, nos diga si es una tautología.
 
+-- E6 Definir una función que dada una fórmula proposicional, nos diga si es una contradicción.
+--
+-- E7 Definir una función que dada una interpretación y una fórmula proposicional, verifique si esta interpretación es un modelo.
+--
+-- E8 Definir una función que dada una fórmula proposicional, esta devuelve la lista de todos sus modelos.
+--
+-- E9 Definir una función que dada una fórmula proposicional, verifica si esta es válida o no.
+--
+-- E10 Definir una función que dada una fórmula proposicional, verifica si esta es insatisfacible.
+--
+-- E11 Definir una función que dada una fórmula proposicional, verifica si esta es satisfacible. 
 
 
 
